0c05581f261934f9b596c5934d57017dc0534602
Fabricio C Zuardi <fabricio@fabricio.org>
2017-07-26 18:28:09 -0300

    make flow happy

--- src/app.js
+++ src/app.js
@@ -1,25 +1,28 @@
 // @flow
-const BASE_URL: string = "https://oodsykqmze.localtunnel.me/";
+const BASE_URL = "https://oodsykqmze.localtunnel.me/";
 // const BASE_URL = '.';
-const SW_URL: string = `${BASE_URL}sw.js`;
+const SW_URL = `${BASE_URL}sw.js`;
 
 console.log({ BASE_URL });
 console.log({ SW_URL });
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker.register(SW_URL).then(
-      function(registration) {
-        // Registration was successful
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
-      },
-      function(err) {
-        // registration failed :(
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
-  });
-}
+window.addEventListener("load", function() {
+  const hasServiceWorker = navigator.serviceWorker === undefined;
+  console.assert(hasServiceWorker);
+  if (navigator.serviceWorker === undefined) {
+    return false;
+  }
+  navigator.serviceWorker.register(SW_URL).then(
+    function(registration) {
+      // Registration was successful
+      console.log(
+        "ServiceWorker registration successful with scope: ",
+        registration.scope
+      );
+    },
+    function(err) {
+      // registration failed :(
+      console.log("ServiceWorker registration failed: ", err);
+    }
+  );
+});
